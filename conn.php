bd99bdb9b87635ad4df1f490ccb0abf79440d7a0
Carlos-Henrique-Jeronimo <152661539+Carlos-Henrique-Jeronimo@users.noreply.github.com>
2025-08-12 19:29:56 -0300

    Create conn.php

---
+++ conn.php
@@ -0,0 +1,13 @@
+<?php
+    $server = 'localhost';
+    $user = 'root';
+    $password = "";
+    $dbname = 'pedemeia_crateus';
+
+    $mysqli = new mysqli($server,$user,$password,$dbname);
+
+    if ($mysqli->error) {
+        die("Falha ao conectar o banco de dados: " . $mysqli->error);
+    }
+    
+?>
