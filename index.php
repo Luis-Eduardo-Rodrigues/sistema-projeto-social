652dee8f2bbafc5907abd22293ad1ab2699d0607
Luis Eduardo Rodrigues <130309482+Luis-Eduardo-Rodrigues@users.noreply.github.com>
2025-08-13 16:38:47 -0300

    Update index.php

--- index.php
+++ index.php
@@ -77,18 +77,16 @@ if(isset($_POST['usuario_s']) AND isset($_POST['senha_s'])){
     }
 
 }
-
-
 ?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Inicial - Sistema de Controle</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <link rel="icon" href="src/projeto.png" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -99,7 +97,6 @@ if(isset($_POST['usuario_s']) AND isset($_POST['senha_s'])){
         }
     </style>
 </head>
-
 <body class="relative">
     <header class="h-40 flex items-center relative">
         <img src="./src/header.png" alt="Imagem Cabeçalho">
@@ -109,22 +106,21 @@ if(isset($_POST['usuario_s']) AND isset($_POST['senha_s'])){
             <p class="font-bold text-black mt-12 text-2xl">SISTEMA DE CONTROLE DO “BOLSA ESTUDANTE CRATÉUS” - PÉ DE MEIA MUNICIPAL</p>
         </div>
         <div class="flex items-center gap-32">
-    <div class="flex items-center justify-center flex-col gap-2 cursor-pointer group" onclick="mostrarModalSecretaria()" id="modalSecretariaBtn">
-        <div class="w-[300px] border-2 border-[#459f6a] rounded-xl p-2 flex items-center justify-center flex-col gap-2 transition-colors duration-300 
+            <div class="flex items-center justify-center flex-col gap-2 cursor-pointer group" onclick="mostrarModalSecretaria()" id="modalSecretariaBtn">
+                <div class="w-[300px] border-2 border-[#459f6a] rounded-xl p-2 flex items-center justify-center flex-col gap-2 transition-colors duration-300 
                     hover:bg-[#357950]">
-            <i class="fa-solid fa-user text-9xl text-[#357950] group-hover:text-white transition-colors duration-300"></i>
-            <p class="font-bold text-[#357950] group-hover:text-white transition-colors duration-300">SECRETARIA DE EDUCAÇÃO</p>
-        </div>
-    </div>
-    <div class="flex items-center justify-center flex-col gap-2 cursor-pointer group" onclick="mostrarModalCoordenador()" id="modalCoordenadorBtn">
-        <div class="w-[300px] border-2 border-[#459f6a] rounded-xl p-2 flex items-center justify-center flex-col gap-2 transition-colors duration-300 
+                    <i class="fa-solid fa-user text-9xl text-[#357950] group-hover:text-white transition-colors duration-300"></i>
+                    <p class="font-bold text-[#357950] group-hover:text-white transition-colors duration-300">SECRETARIA DE EDUCAÇÃO</p>
+                </div>
+            </div>
+            <div class="flex items-center justify-center flex-col gap-2 cursor-pointer group" onclick="mostrarModalCoordenador()" id="modalCoordenadorBtn">
+                <div class="w-[300px] border-2 border-[#459f6a] rounded-xl p-2 flex items-center justify-center flex-col gap-2 transition-colors duration-300 
                     hover:bg-[#357950]">
-            <i class="fa-solid fa-user text-9xl text-[#357950] group-hover:text-white transition-colors duration-300"></i>
-            <p class="font-bold text-[#357950] group-hover:text-white transition-colors duration-300">COORDENADOR(A)</p>
+                    <i class="fa-solid fa-user text-9xl text-[#357950] group-hover:text-white transition-colors duration-300"></i>
+                    <p class="font-bold text-[#357950] group-hover:text-white transition-colors duration-300">COORDENADOR(A)</p>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-
     </main>
     <footer class="w-full flex items-center py-2 justify-around mt-24 border-t-2 border-[#357950]">
         <div>
@@ -137,30 +133,46 @@ if(isset($_POST['usuario_s']) AND isset($_POST['senha_s'])){
             <img src="./src/logo-estado.png" class="w-40" alt="Logo do Estado">
         </div>
     </footer>
+    <section id="modalSecretaria" class="flex items-center justify-center fixed inset-0 backdrop-blur-sm bg-black/40 opacity-0 scale-95 pointer-events-none transition-all duration-300 z-50">
+        <form action="" method="post" class="w-[22rem] bg-[#4bac72] rounded-lg relative shadow-xl">
+            <h2 class="text-2xl font-bold text-center my-4 text-white">LOGIN</h2>
+            <button type="button" onclick="fecharModalSecretaria()" class="absolute right-2 top-2 w-9 h-9 flex items-center justify-center rounded-md cursor-pointer bg-white text-black hover:bg-red-500 hover:text-white transition-colors duration-300">
+                <i class="fa-solid fa-xmark"></i>
+            </button>
+            <div class="bg-[#4bac72] shadow-md p-4 space-y-4">
+                <label for="usuario" class="block text-sm text-white mb-1">Usuário</label>
+                <input id="usuario" name="usuario_s" type="text" class="shadow-lg py-2 px-3 focus:outline-none w-full bg-gray-50 border border-white rounded-md">
+            </div>
+            <div class="bg-[#4bac72] shadow-md p-4">
+                <label for="senha" class="block text-sm text-white mb-1">Senha</label>
+                <input id="senha" name="senha_s" type="password" class="shadow-lg py-2 px-3 focus:outline-none w-full bg-gray-50 border border-white rounded-md">
+            </div>
+            <div class="flex items-center justify-center">
+                <button type="submit" class="w-full text-sm bg-[#357950] transition cursor-pointer hover:bg-green-900 px-5 rounded-md p-2 text-white font-bold m-3 border-2 border-[#cfb426] text-lg">
+                    Entrar
+                </button>
+            </div>
+        </form>
+    </section>
+    <section id="modalCoordenador" class="flex items-center justify-center fixed inset-0 backdrop-blur-sm bg-black/40 opacity-0 scale-95 pointer-events-none transition-all duration-300 z-50">
+        <form action="" method="post" class="w-[22rem] bg-[#4bac72] rounded-lg relative shadow-xl">
 
+            <h2 class="text-2xl font-bold text-center my-4 text-white">LOGIN</h2>
+            <button type="button" onclick="fecharModalCoordenador()" class="absolute right-2 top-2 w-9 h-9 flex items-center justify-center rounded-md cursor-pointer bg-white text-black hover:bg-red-500 hover:text-white transition-colors duration-300">
+                <i class="fa-solid fa-xmark"></i>
+            </button>
 
+            <div class="bg-[#4bac72] shadow-md p-4 space-y-4">
+                <label for="usuario_coordenador" class="block text-sm text-white mb-1">Usuário</label>
+                <input id="usuario_coordenador" name="usuario_c" type="text" class="shadow-lg py-2 px-3 focus:outline-none w-full bg-gray-50 border border-white rounded-md">
+            </div>
 
+            <div class="bg-[#4bac72] shadow-md p-4">
+                <label for="senha_coordenador" class="block text-sm text-white mb-1">Senha</label>
+                <input id="senha_coordenador" name="senha_c" type="password" class="shadow-lg py-2 px-3 focus:outline-none w-full bg-gray-50 border border-white rounded-md">
+            </div>
 
-
-    <!-- modais -->
-
-    <!--coordenador-->
-    <section class="flex items-center flex-col gap-6 p-4 absolute top-50 left-150 w-72 hidden" id="modalCoordenador">
-        <form action="" method="post">
-            <div class="w-80 bg-[#5cd65c] rounded-md ">
-                <h2 class="text-lg font-bold text-center mb-4 text-white">LOGIN</h2>
-
-                <div class="bg-[#5cd65c] shadow-md  p-4 space-y-4" >
-                    <label for="usuario" class="block text-sm text-white mb-1">Usuário</label>
-                    <input id="usuario" name="usuario_c" type="text" class="shadow-lg w-full bg-gray-50 border border-white rounded-md ">
-                </div>
-
-                <div class="bg-[#5cd65c] shadow-md p-4">
-                    <label for="senha" class="block text-sm text-white mb-1">Senha</label>
-                    <input id="senha" name="senha_c" type="text" class="shadow-lg w-full bg-gray-50 border border-white rounded-md ">
-                </div>
-
-                <div class=" bg-[#5cd65c] shadow-md p-4">
+                            <div class=" bg-[#5cd65c] shadow-md p-4">
                     <select name="escola" id="escola" class="block text-sm text-white mb-1">
                     <option value="" class="shadow-lg w-full bg-gray-50 border border-white rounded-md">Escolha sua escola</option>
                      <?php
@@ -168,8 +180,6 @@ if(isset($_POST['usuario_s']) AND isset($_POST['senha_s'])){
                         $escolas = mysqli_query($mysqli, $sql);
                         foreach($escolas as $escola){
                     ?>
-                    
-                    
                     <option value="<?=$escola['nome_escola']?>" class="shadow-lg text-black w-full bg-gray-50 border border-white rounded-md"><?=$escola['nome_escola']?></option>
                     <?php
                         }
@@ -177,67 +187,34 @@ if(isset($_POST['usuario_s']) AND isset($_POST['senha_s'])){
                 </select>
                 </div>
 
-                <div class="text-right ">
-                    <button type="submit" class="w-full text-sm bg[#1e7b1e] hover:bg-green-600 px-5 rounded-md p-2 text-white font-bold">Entrar</button>
-                </div>
+            <div class="flex items-center justify-center">
+                <button type="submit" class="w-full text-sm bg-[#357950] transition cursor-pointer hover:bg-green-900 px-5 rounded-md p-2 text-white font-bold m-3 border-2 border-[#cfb426] text-lg">
+                    Entrar
+                </button>
             </div>
-        
-        </form>        
-        
-    
-    </section>
-
-    <!--secretaria-->
-     <section class="flex items-center flex-col gap-6 p-4 absolute top-50 left-150 w-72 hidden" id="modalSecretaria">
-        <form action="" method="post">
-            <div class="w-80 bg-[#5cd65c] rounded-md ">
-            <h2 class="text-lg font-bold text-center mb-4 text-white">LOGIN</h2>
-
-            <div class="bg-[#5cd65c] shadow-md  p-4 space-y-4" >
-                <label for="usuario" class="block text-sm text-white mb-1">Usuário</label>
-                <input id="usuario" name="usuario_s" type="text" class="shadow-lg w-full bg-gray-50 border border-white rounded-md ">
-            </div>
-
-            <div class="bg-[#5cd65c] shadow-md p-4">
-                <label for="senha" class="block text-sm text-white mb-1">Senha</label>
-                <input id="senha" name="senha_s" type="text" class="shadow-lg w-full bg-gray-50 border border-white rounded-md ">
-            </div>
-
-            <div class="text-right ">
-                <button type="submit" class="w-full text-sm bg[#1e7b1e] hover:bg-green-600 px-5 rounded-md p-2 text-white font-bold">Entrar</button>
-            </div>
-
         </form>
-        
-    </div>
-    
     </section>
-
-
     <script>
-        function mostrarModalCoordenador(){
-            const modalCoordenador = document.getElementById("modalCoordenador")
-            modalCoordenador.classList.remove("hidden")
+        function mostrarModalSecretaria() {
+            const modal = document.getElementById("modalSecretaria");
+            modal.classList.remove("pointer-events-none", "opacity-0", "scale-95");
+            modal.classList.add("opacity-100", "scale-100");
         }
-
-        function fecharModalCoordenador(){
-            const modalCoordenador = document.getElementById("modalCoordenador")
-
-            modalCoordenador.classList.add("hidden")
+        function fecharModalSecretaria() {
+            const modal = document.getElementById("modalSecretaria");
+            modal.classList.remove("opacity-100", "scale-100");
+            modal.classList.add("opacity-0", "scale-95", "pointer-events-none");
         }
-
-
-        function mostrarModalSecretaria(){
-            const modalSecretaria = document.getElementById("modalSecretaria")
-            modalSecretaria.classList.remove("hidden")
+        function mostrarModalCoordenador() {
+            const modal = document.getElementById("modalCoordenador");
+            modal.classList.remove("pointer-events-none", "opacity-0", "scale-95");
+            modal.classList.add("opacity-100", "scale-100");
         }
-
-        function fecharModalSecretaria(){
-            const modalSecretaria = document.getElementById("modalSecretaria")
-            modalSecretaria.classList.add("hidden")
+        function fecharModalCoordenador() {
+            const modal = document.getElementById("modalCoordenador");
+            modal.classList.remove("opacity-100", "scale-100");
+            modal.classList.add("opacity-0", "scale-95", "pointer-events-none");
         }
     </script>
 </body>
-
-
 </html>
